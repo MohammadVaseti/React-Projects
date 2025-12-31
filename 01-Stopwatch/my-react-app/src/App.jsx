370af15c885f5b47e13419431fdb5aae395cd5f8
mohammad vaseti <mohammadvaseti1998@gmail.com>
2025-12-31 16:13:33 +0330

    repo updated

--- 01-Stopwatch/my-react-app/src/App.jsx
+++ 01-Stopwatch/my-react-app/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
   return (
     <>
       <h1>Stopwatch</h1>
+      <div>
+        <span>{"0" + (Math.floor(time / 60000) % 60)}</span>
+        <span>{"0" + (Math.floor(time / 1000) % 60)}</span>
+        <span>{"0" + ((time / 10) % 100)}</span>
+      </div>
+      <button onClick={setRunning(true)}>Start</button>
+      <button onClick={setRunning(false)}>Stop</button>
+      <button onClick={setTime(0)}>Reset</button>
     </>
   );
 }
