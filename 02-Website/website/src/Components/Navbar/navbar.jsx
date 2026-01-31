f649d8b4089f9ae7666f3f7295a2386f52d57d75
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-01-31 16:48:25 +0330

    repo updated

--- 02-Website/website/src/Components/Navbar/navbar.jsx
+++ 02-Website/website/src/Components/Navbar/navbar.jsx
@@ -1,9 +1,13 @@
-import "./navbar.css";
+import styled from "./navbar.module.css";
 
 function Navbar() {
   return (
-    <div>
-      <h1 className="header">Header</h1>
+    <div className={styled.headerContainer}>
+      <ul>
+        <li>Blog Lists</li>
+        <li>New Blog</li>
+        <li>About Us</li>
+      </ul>
     </div>
   );
 }
