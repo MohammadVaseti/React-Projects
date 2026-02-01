045635afede308b91f1d28dbac06fde07bf8a5a5
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-01 11:50:18 +0330

    repo updated

---
+++ 02-Website/website/src/Components/article/Article.jsx
@@ -0,0 +1,10 @@
+import styled from "../article/articleStyle.module.css";
+function Article() {
+  return (
+    <>
+      <div className={styled.article}>article</div>
+    </>
+  );
+}
+
+export default Article;
--- 02-Website/website/src/pages/home/homes.jsx
+++ 02-Website/website/src/pages/home/homes.jsx
@@ -1,9 +1,14 @@
+import Article from "../../Components/article/Article";
 import Navbar from "../../Components/Navbar/navbar";
-
+import styles from "../home/homeStyle.module.css";
 function Home() {
   return (
     <>
       <Navbar title="My Blog" />
+      <div className={styles.container}>
+        <h2>New Blogs</h2>
+        <Article />
+      </div>
     </>
   );
 }
