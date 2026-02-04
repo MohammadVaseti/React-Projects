5c3eb6294d5f8573cf1fd163dc29ec3214575026
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-04 10:42:04 +0330

    repo updated

--- 02-Website/website/src/Components/article/Article.jsx
+++ 02-Website/website/src/Components/article/Article.jsx
@@ -1,12 +1,17 @@
 import styled from "../article/articleStyle.module.css";
 import pic from "../../assets/images/images.png";
-function Article() {
+function Article(props) {
+  console.log(props);
   return (
     <>
       <div className={styled.article}>
-        <img src={pic} alt="react" className={styled.picture}></img>
-        <h3>React Lessons</h3>
-        <span>5m</span>
+        <img
+          src={props.data.imgUrl}
+          alt="react"
+          className={styled.picture}
+        ></img>
+        <h3>{props.data.title}</h3>
+        <span>{props.data.time}m</span>
       </div>
     </>
   );
