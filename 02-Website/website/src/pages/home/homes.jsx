0110bd4e330cf49a431760ced855af2daf8dac9e
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-04 10:00:20 +0330

    repo updated

--- 02-Website/website/src/pages/home/homes.jsx
+++ 02-Website/website/src/pages/home/homes.jsx
@@ -2,6 +2,15 @@ import Article from "../../Components/article/Article";
 import Navbar from "../../Components/Navbar/navbar";
 import styles from "../home/homeStyle.module.css";
 function Home() {
+  let articles = [
+    {
+      id: 1,
+      imgUrl: "",
+      title: "first",
+      time: 5,
+    },
+  ];
+
   return (
     <>
       <Navbar title="My Blog" />
