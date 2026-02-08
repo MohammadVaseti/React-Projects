63119300a39619e251a105f60e2b901b26ea00ee
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-08 15:16:31 +0330

    repo updated

--- 02-Website/website/src/Components/Navbar/navbar.jsx
+++ 02-Website/website/src/Components/Navbar/navbar.jsx
@@ -1,13 +1,20 @@
 import styled from "./navbar.module.css";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
   return (
     <div className={styled.headerContainer}>
       <h4>{props.title}</h4>
       <ul>
-        <li>Blog Lists</li>
-        <li>New Blog</li>
-        <li>About Us</li>
+        <li>
+          <Link to="/">Blog Lists</Link>
+        </li>
+        <li>
+          <Link>New Blog</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
       </ul>
     </div>
   );
--- 02-Website/website/src/pages/aboutUs/About.jsx
+++ 02-Website/website/src/pages/aboutUs/About.jsx
@@ -1,7 +1,23 @@
+import Navbar from "../../Components/Navbar/navbar";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <Navbar title="My Blog" />
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        repudiandae fugit quasi accusamus nisi sunt recusandae eos error
+        corrupti iusto tempore, vel itaque hic iste, aspernatur voluptate eius
+        voluptates sed consequuntur officiis. Magni, suscipit. Hic ipsam sint
+        itaque reiciendis architecto, necessitatibus esse, odio nesciunt id
+        porro deleniti asperiores? Corrupti ullam ad iusto ipsum fugit esse rem
+        vel magnam cum iure, velit atque, repellat obcaecati ipsam amet
+        inventore error? At iste a cupiditate dignissimos repellat ipsam
+        molestiae impedit consequuntur suscipit velit, id reprehenderit
+        doloremque ut hic asperiores mollitia accusantium quia architecto
+        quibusdam optio debitis quo explicabo? Eos nulla praesentium ducimus
+        molestias.
+      </p>
     </div>
   );
 };
