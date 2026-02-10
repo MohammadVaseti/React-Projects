7cabec96fc3f2f6f1a245612cecd21d0fdfae4eb
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-10 09:18:38 +0330

    repo updated

--- 02-Website/website/src/pages/articlePage/ArticlePage.jsx
+++ 02-Website/website/src/pages/articlePage/ArticlePage.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 import Footer from "../../Components/Footer/footer";
 import Navbar from "../../Components/Navbar/navbar";
 import styled from "../articlePage/articlePage.module.css";
 
 const ArticlePage = () => {
+  const param = useParams();
+
   return (
     <>
       <Navbar title="My Blog"></Navbar>
@@ -15,8 +18,18 @@ const ArticlePage = () => {
         </div>
         <img src="../../src/assets/images/iran.png" alt="" />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In illo magni
-          tenetur numquam at error! Ipsa eaque expedita soluta praesentium?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A est ipsam
+          maxime atque iusto fugit, consectetur cupiditate saepe quasi expedita,
+          doloremque vel neque rem veniam enim laborum aperiam ad quo non
+          voluptatum excepturi! Rem, reprehenderit quis odit a, recusandae
+          tempore illo exercitationem quidem, quos quas nisi eos fugiat! Minus
+          natus dignissimos asperiores consectetur! Quidem porro voluptatum
+          molestias, officiis ea ut rem quia obcaecati earum temporibus
+          inventore quis minima ullam, fuga tempore necessitatibus cumque animi
+          nihil libero, expedita fugit doloremque ipsa dignissimos quasi. Modi
+          facilis, at excepturi harum repellendus libero expedita itaque odio
+          inventore labore, qui praesentium corrupti! Autem, maxime
+          consequuntur.
         </p>
       </div>
       <Footer></Footer>
