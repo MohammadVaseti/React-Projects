45f0541af567d9bfe5f8e57089c9bf12cda8ce3e
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-12 11:00:03 +0330

    repo updated

--- 02-Website/website/src/App.jsx
+++ 02-Website/website/src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./pages/aboutUs/About";
 import ArticlePage from "./pages/articlePage/ArticlePage";
+import CreateArticle from "./pages/createArticle/CreateAricle";
 import Home from "./pages/home/homes";
 import { Route, Routes } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path="/article/create-article" element={<CreateArticle />} />
       </Routes>
     </>
   );
