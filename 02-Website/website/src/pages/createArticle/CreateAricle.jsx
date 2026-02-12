341c148902d738bb32f216cdc892ccea1ccaa2f4
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-12 14:52:48 +0330

    repo updated

--- 02-Website/website/src/pages/createArticle/CreateAricle.jsx
+++ 02-Website/website/src/pages/createArticle/CreateAricle.jsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
 import Footer from "../../Components/Footer/footer";
 import Navbar from "../../Components/Navbar/navbar";
 import styled from "../createArticle/createArticle.module.css";
 const CreateArticle = () => {
+  const [article, setArticle] = useState({
+    title: "",
+    date: "",
+  });
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case "title":
+        setArticle({
+          title: e.target.value,
+        });
+
+        break;
+
+      case "date":
+        setArticle({
+          date: e.target.value,
+        });
+
+        break;
+    }
+    console.log(article);
+  };
+
   return (
     <>
       <Navbar />
       <h1>Create Article</h1>
+      <div>
+        <label>title</label>
+        <input name="title" onChange={handleChange} type="text"></input>
+      </div>
+      <div>
+        <label>Date</label>
+        <input name="date" onChange={handleChange} type="text"></input>
+      </div>
       <Footer />
     </>
   );
