d6ca5924f8ec95a2efff8c155f49d766a0f5ed07
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-12 10:56:28 +0330

    repo updated

---
+++ 02-Website/website/src/pages/createArticle/CreateAricle.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../Components/Footer/footer";
+import Navbar from "../../Components/Navbar/navbar";
+import styled from "../createArticle/createArticle.module.css";
+const CreateArticle = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Create Article</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default CreateArticle;
