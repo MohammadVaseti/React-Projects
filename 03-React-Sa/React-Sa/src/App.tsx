c35f13658378272a3bc6d4544137b158c51e39f8
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-03 11:32:48 +0330

    repo updated

--- 03-React-Sa/React-Sa/src/App.tsx
+++ 03-React-Sa/React-Sa/src/App.tsx
@@ -2,6 +2,8 @@ import User from "./components/User";
 import "./styles/style.css";
 import styles from "./styles/style.module.css";
 const App = () => {
+  const names = ["saadoun", "maajoun"];
+
   return (
     <>
       <div>
@@ -9,6 +11,9 @@ const App = () => {
         <h1 className="color">hello</h1>
         <h2 className={styles.back}>hi</h2>
         <h3 style={{ color: "yellow", backgroundColor: "green" }}>good</h3>
+        {names.map((v, i) => {
+          return <h1>{v}</h1>;
+        })}
       </div>
     </>
   );
