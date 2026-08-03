2e6a140aae84a4a578c3d5f8587073ce1b91c5b8
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-03 11:04:53 +0330

    repo updated

--- 03-React-Sa/React-Sa/src/App.tsx
+++ 03-React-Sa/React-Sa/src/App.tsx
@@ -1,11 +1,13 @@
 import User from "./components/User";
 import "./styles/style.css";
+import styles from "./styles/style.module.css";
 const App = () => {
   return (
     <>
       <div>
         <User name="saadoun" age={17}></User>
         <h1 className="color">hello</h1>
+        <h2 className={styles.back}>hi</h2>
       </div>
     </>
   );
