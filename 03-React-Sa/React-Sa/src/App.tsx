a35b3989940a1dcf4972ba0993c14fdfd15962a3
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-03 11:22:29 +0330

    repo updated

--- 03-React-Sa/React-Sa/src/App.tsx
+++ 03-React-Sa/React-Sa/src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
         <User name="saadoun" age={17}></User>
         <h1 className="color">hello</h1>
         <h2 className={styles.back}>hi</h2>
+        <h3 style={{ color: "yellow", backgroundColor: "green" }}>good</h3>
       </div>
     </>
   );
