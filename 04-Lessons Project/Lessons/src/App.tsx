dfd8eb73497aa136c8b04b589831ea41ec5bbbbd
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-08-05 11:59:38 +0330

    repo updated

--- 04-Lessons Project/Lessons/src/App.tsx
+++ 04-Lessons Project/Lessons/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
-const [courseList, setCourseList] = useState();
+const [courseList, setCourseList] = useState([]);
+const [newCourse, setNewCourse] = useState("");
 
 function App() {
+  const handleInput = () => {};
+
   return (
     <>
       <div className="add-course">
-        <input type="text"></input>
+        <input type="text" onChange={handleInput}></input>
         <button type="button">Add Course</button>
       </div>
       <div className="list"></div>
